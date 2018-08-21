d5772bac3fa7ffd22f1d75ccaa7b1018775b4f96
jason rukman <unclej@gmail.com>
2018-08-20 17:10:13 -0700

    Phase 5 - Freeze Tag
    Same concept as tag but we track player states so we can have players frozen or not.

--- tag.js
+++ tag.js
@@ -3,39 +3,39 @@
 var cwk = require('cwk');
 var utils = require('utils');
 
-var whoIsIt;
-var intervalId;
-var gameTime;
+var PLAYER = 0, FROZEN = 1, IT = 2;
+var playerStates = {};	// initial empty object
 
-function setPlayerIt(player) {
-	echo(player, "TAG! You're IT!");
-	cwk.equipArmor(player, "GOLD");
-	whoIsIt = player.getDisplayName();
+function getPlayerState(player) {
+	return playerStates[player.getDisplayName()];
 }
 
-function update() {
-	gameTime -= 5;
-	if (gameTime == 0) {
-		clearInterval(intervalId);
-		utils.players(function(player) {
-			player.sendTitle("Game over!", 
-				whoIsIt + " LOST!", 
-				10, 300, 20);
-		});
-	} 
+// if no one is a PLAYER anymore than we have a winner
+function didWin()
+{
+	for (var player in playerStates) {
+		if (playerStates[player] == PLAYER) return false;
+	}
+	return true;
+}
+
+function setPlayerState(player, state) {
+	playerStates[player.getDisplayName()] = state;
+	if (state == IT) {
+		echo(player, "TAG! You're IT!");
+		cwk.equipArmor(player, "GOLD");
+	}
+	else if (state == FROZEN) {
+		cwk.equipArmor(player, "DIAMOND");
+	}
 	else {
-		utils.players(function(player) {
-			echo(player, gameTime + " secs left!");
-		});
+		cwk.stripArmor(player);
 	}
 }
 
-command('tag', function(parameters, player) {
-
-	gameTime = 60;
-
+command('tag', function(parameters, player) { 
 	var drone = new Drone(player);
-	drone.arena(blocks.gold, 50, 10, 50);
+	drone.arena(blocks.diamond, 50, 10, 50);
 
 	var players = utils.players(function(other) {
 		other.teleport(player);	
@@ -43,21 +43,46 @@ command('tag', function(parameters, player) {
 			echo(other, "You have to be in survival to play!");
 		}
 		else {
-			cwk.stripArmor(other);
+			setPlayerState(other, PLAYER);
 		}
 	});
 
     var it = players[cwk.random(0, players.length-1)];
-    setPlayerIt(it);
+    setPlayerState(it, IT);
 
-	cwk.pvpDamage(function(whoHit, whoWasHit, cancel) {
-		if (whoHit.getDisplayName() == whoIsIt) {
-			setPlayerIt(whoWasHit);
-			cwk.stripArmor(whoHit);
-    	}
-		cancel();
+	events.playerMove( function(e) { 
+		if (getPlayerState(e.getPlayer()) == FROZEN) {
+			// if frozen set to to from and allow look around
+			var to = e.getFrom();
+			to.setPitch(e.getTo().getPitch());
+			to.setYaw(e.getTo().getYaw());
+			e.setTo(to); 
+		}
 	});
 
-	intervalId = setInterval(update, 5000);
-	update(); // Do the first update
+	cwk.pvpDamage(function(whoHit, whoWasHit, cancel) {
+
+		if (getPlayerState(whoWasHit) == FROZEN 
+			&& getPlayerState(whoHit) == PLAYER) 
+		{
+			setPlayerState(whoWasHit, PLAYER);
+		}
+		else if (getPlayerState(whoWasHit) == PLAYER
+			&& getPlayerState(whoHit) == IT) 
+		{
+			setPlayerState(whoWasHit, FROZEN);
+
+			if (didWin()) {
+				utils.players(function(player) {
+					player.sendTitle("Game over!", 
+					whoHit.getDisplayName() + " WON!", 
+					10, 300, 20);
+				// can do this if you want everyone back to normal straight away
+				// setPlayerState(player, PLAYER);
+				});
+				playerStates = {};
+			} 
+		}
+		cancel();
+	});
 });
